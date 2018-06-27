d4941589f6ae59cff62604c70c7761f22b834cf5
Niklas Goby <ngoby@geospin.de>
2018-06-27 18:23:04 +0200

    add nohup to experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,3 +1,5 @@
+nohup python -m visdom.server 1>visdom_out.log &
+
 # c1
  python main.py \
    --env-name "ng_Worker" \
@@ -15,7 +17,7 @@
    --save-model-postfix "c1"
 
   # c2
- python main.py \
+  nohup python main.py \
    --env-name "ng_Worker" \
    --algo ppo \
    --use-gae \
@@ -29,10 +31,11 @@
    --vis-interval 1 \
    --log-interval 10 \
    --ppo-epoch 32 \
-   --save-model-postfix "c2"
+   --save-model-postfix "c2" \
+   1>c2_out.log 2>c2_err.log &
 
     # c3 - single round for testing
- python main.py \
+ nohup python main.py \
    --env-name "ng_Worker" \
    --algo ppo \
    --use-gae \
@@ -46,4 +49,5 @@
    --vis-interval 1 \
    --log-interval 10 \
    --ppo-epoch 32 \
-   --save-model-postfix "c3"
+   --save-model-postfix "c3" \
+   1>c3_out.log 2>c3_err.log &
